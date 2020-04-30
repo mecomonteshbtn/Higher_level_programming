1b82a5472297a0b5433a60b0ddd3da30493490eb
Robinson Montes <1574@holbertonschool.com>
2020-04-30 18:15:59 +0000

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+import sys
+
+if __name__ != "__main__":
+    exit()
+
+i = 0
+result = 0
+for argument in sys.argv:
+    if i != 0:
+        result += int(argument)
+    else:
+        i += 1
+print("{:d}".format(result))
