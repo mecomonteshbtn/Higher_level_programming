d555432fae2d9e59838e8bd90b50674f4f63c559
Robinson Montes <1574@holbertonschool.com>
2020-04-30 19:19:47 +0000

    corrections on task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -17,7 +17,7 @@ elif argv[2] == '*':
 elif argv[2] == '/':
     result = div(int(argv[1]), int(argv[3]))
 else:
-    print("Unknown operator. Available operators: +, -, *, and /")
+    print("Unknown operator. Available operators: +, -, * and /")
     exit(1)
 
 print("{:s} {:s} {:s} = {:d}".format(argv[1], argv[2], argv[3], result))
