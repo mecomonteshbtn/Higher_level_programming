c78bbfa1d1a04f38aa65eef16b6d2ccf40ddc958
Robinson Montes <1574@holbertonschool.com>
2020-04-30 17:09:07 +0000

    tasks 0,1 and 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+import sys
+
+if __name__ != "__main__":
+    exit()
+
+argumentStr = "{:d} argument"
+argc = len(sys.argv) - 1
+if argc == 0:
+    argumentStr += 's.'
+elif argc == 1:
+    argumentStr += ':'
+else:
+    argumentStr += 's:'
+print(argumentStr.format(argc))
+
+i = 0
+for argument in sys.argv:
+    if i != 0:
+        print("{:d}: {:s}".format(i, argument))
+    i += 1
