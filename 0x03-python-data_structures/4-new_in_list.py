aa4096916682efa6a1f7e29aed61ddca506af693
Robinson Montes <1574@holbertonschool.com>
2020-05-05 17:06:58 +0000

    corrections on 4 and 6

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,10 +5,9 @@ def new_in_list(my_list, idx, element):
     """
     Replaces an element in a list at a specific position
     Without modifying the original list
-    """    
+    """
     if idx >= len(my_list) or idx < 0:
         return (my_list)
-
     new_list = my_list.copy()
     new_list[idx] = element
     return (new_list)
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -11,5 +11,5 @@ def print_matrix_integer(matrix=[[]]):
             if i != row_len - 1:
                 print("{:d}".format(row[i]), end=' ')
             else:
-                print("{:d}".format(row[i]), end='')    
-        print()
+                print("{:d}".format(row[i]), end='')
+	print()
