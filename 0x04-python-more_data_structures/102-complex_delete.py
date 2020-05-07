9ad89e5693d616cce4055fe7c52d4ed48995a1a6
Robinson Montes <1574@holbertonschool.com>
2020-05-07 21:30:47 +0000

    add delete to 102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,5 +2,11 @@
 
 
 def complex_delete(a_dictionary, value):
-    new_dict = {k: v for k, v in a_dictionary.items() if v != value}
-    return (new_dict)
+    """
+    Delete keys with a specific value in a dictionary
+    """
+    if a_dictionary is not None and type(a_dictionary) is dict:
+        to_delete = {k: v for k, v in a_dictionary.items() if v == value}
+        for key, val in to_delete.items():
+            del a_dictionary[key]
+    return (a_dictionary.copy())
