b230f752b75a5f0f36c7787d87a2ab8984b8f379
Robinson Montes <1574@holbertonschool.com>
2020-05-07 18:37:08 +0000

    format corrections on 6 and 11

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
-
 def mutiply_list_map(my_list=[], number=0):
-    """
-    return a list with all values multiplied by a number using map
-    """
     return (list(map(lambda x: x*number, my_list)))
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -6,4 +6,4 @@ def print_sorted_dictionary(a_dictionary):
     prints a dictionary by ordered keys. keys assumed to be strings
     """
     [print("{}: {}".format(key, a_dictionary[key]))
-	for key in sorted(a_dictionary)]
+        for key in sorted(a_dictionary)]
