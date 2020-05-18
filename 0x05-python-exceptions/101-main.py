4998bcb618fd1719c555184d7d3dc58862134b6f
Robinson Montes <1574@holbertonschool.com>
2020-05-18 19:56:14 +0000

    Task 101

--- 0x05-python-exceptions/101-main.py
+++ 0x05-python-exceptions/101-main.py
@@ -21,4 +21,3 @@ def print_list(my_list, len):
 
 result = safe_function(print_list, [1, 2, 3, 4], 10)
 print("result of print_list: {}".format(result))
-
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+
+
 def safe_function(fct, *args):
+    """
+    executes a function safely and return the result of the function
+    """
+    try:
+        result = fct(*args)
+        return result
+    except Exception as e:
+        import sys
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
