5783f5caf093093abc99d6ddef3e94cf14ee55b4
Robinson Montes <1574@holbertonschool.com>
2020-05-18 19:57:44 +0000

    task 102

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 void print_python_list(PyObject *p);
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += (a ** b) / i
+        except:
+            result = b + a
+            break
+    return result
