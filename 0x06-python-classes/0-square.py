335c18e80c47bed65dd18d4e3f3e23991605d7ed
Robinson Montes <1574@holbertonschool.com>
2020-05-19 21:04:45 +0000

    correction on task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 14:21:54 2020
 
+@author: meco
+"""
 
+"""Create a class"""
 class Square:
-    """empty class Square that defines a square
-    """
+    """empty class Square that defines a square"""
     pass
