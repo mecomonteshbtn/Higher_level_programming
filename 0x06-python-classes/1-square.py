14aac20a1118d90d66ef0eae0c3383c76d52f78a
Robinson Montes <1574@holbertonschool.com>
2020-05-19 21:41:42 +0000

    corrections on task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 16:23:37 2020
+
+@author: meco
+"""
 
 
 class Square:
-    """
-    empty class for Square
+    """Define a class for Square
+
+    Instatation with size of the square
+
+    Attributes:
+        size: a private instance
     """
     def __init__(self, size):
-        """
-        Instatation with sizewith a private instance attribute size
-        """
         self.__size = size
