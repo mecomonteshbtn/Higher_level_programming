57016d3a01db650ac5baee57600d722ec5399cc5
Robinson Montes <1574@holbertonschool.com>
2020-05-24 17:35:03 +0000

    a new attempt

--- 0x07-python-test_driven_development/101-main.py
+++ 0x07-python-test_driven_development/101-main.py
@@ -3,3 +3,5 @@ lazy_matrix_mul = __import__('101-lazy_matrix_mul').lazy_matrix_mul
 
 print(lazy_matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
 print(lazy_matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
+print(lazy_matrix_mul([[3, 4], [6, 5]], [[8], [7]]))
+print(lazy_matrix_mul([[3.6, -4.5], [-6, 5-3]], [[-0.8], [1.7]]))
