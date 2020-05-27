c38c4153adc0781a07afd2104598719126dc039e
Robinson Montes <1574@holbertonschool.com>
2020-05-27 00:23:05 +0000

    fix 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 25 2020
+@author: Robinson Montes
+"""
+
+
 class LockedClass:
     """A locked class that only lets the user dynamically create the instance
     attribute 'first_name'"""
