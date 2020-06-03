68f01de9471dfc54092ffb2f1b7d47bb65377b24
Robinson Montes <1574@holbertonschool.com>
2020-06-03 01:01:44 +0000

    task 100

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 29 15:13:37 2020
+
+@author: Robinson Montes
+"""
+
+
+class MyInt(int):
+    """
+    A class that inherits from int
+    """
+    def __eq__(self, num):
+        """
+        equal function for MyInt class
+
+        Attributes:
+            num (int): an inputed integer
+
+        Returns:
+            The opositive booleanof the input
+        """
+        return (int(self) != num)
+
+    def __ne__(self, num):
+        """
+        no equal function for MyInt class
+
+        Attributes:
+            num (int): an inputed integer
+
+        Returns:
+            The opositive booleanof the input
+        """
+        return (int(self) == num)
