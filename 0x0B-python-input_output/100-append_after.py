c47aabb830c273863c9c466b6bc1cd3628d39be4
Robinson Montes <1574@holbertonschool.com>
2020-06-04 01:16:06 +0000

    adding argument filename into task 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -12,9 +12,9 @@ def append_after(filename="", search_string="", new_string=""):
     Inserts a line after each line containing a specific string
 
     Arguments:
+        filename (str): The name of the file
         search_string (str): The string to math
         new_string (str): The string to insert after matching
-
     """
     with open(filename, 'r', encoding='utf-8') as file:
         lines = file.readlines()
@@ -22,6 +22,7 @@ def append_after(filename="", search_string="", new_string=""):
     for i in range(len(lines)):
         if search_string in lines[i]:
             lines.insert(i + 1, new_string)
+
     with open(filename, 'w', encoding='utf-8') as file:
         content = "".join(lines)
         file.write(content)
