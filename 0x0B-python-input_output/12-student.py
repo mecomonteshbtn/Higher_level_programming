d860e0c781a4eec69211357e0a17a936e3d87777
Robinson Montes <1574@holbertonschool.com>
2020-06-05 15:03:44 +0000

    fix 12

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tru Jun 3 15:13:37 2020
+@author: Robinson Montes
+"""
+
+
+class Student:
+    """
+    Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        init method for Student class
+
+        Attributes:
+            first_name (str): The first name of the student
+            last_name (str): The last name of the student
+            age (int): The age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Represents of Student into json format
+
+        Return:
+            Student class as a json format
+        """
+        if attrs is None:
+            return self.__dict__
+        return {key: value for key, value in self.__dict__.items()
+                if key in attrs}
