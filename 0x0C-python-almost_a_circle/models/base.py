4963de6f489035243c1ca3c5193891aa8665a01b
Robinson Montes <1574@holbertonschool.com>
2020-06-09 22:01:41 +0000

    Fix errors

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -91,7 +91,7 @@ class Base:
         """
         if cls.__name__ == "Rectangle":
             dummy = cls(1, 1)
-        elif cls.__name__ == "Square":
+        if cls.__name__ == "Square":
             dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
