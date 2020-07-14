61d3e4d413a65ebb8533d7aa1d5d4ec9ce9777b8
Robinson Montes <1574@holbertonschool.com>
2020-07-14 23:36:13 +0000

    List all privileges for user_0d_1 and user_0d_2 in localhost

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- List privileges using SHOW GRANTS
+-- DML QUERY to list all privileges for user_0d_1 and user_0d_2 in localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
