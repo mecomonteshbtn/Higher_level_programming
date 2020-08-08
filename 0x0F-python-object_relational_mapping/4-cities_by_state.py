219730648672e6200932e5131ea05d758d4b9193
Robinson Montes <1574@holbertonschool.com>
2020-08-08 22:34:05 +0000

    filter cities and state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""
+Created on Sat Aug  8 09:05:11 2020
+
+@author: Robinson Montes
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 4:
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
+    username = args[1]
+    password = args[2]
+    data = args[3]
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=data, port=3306)
+    cur = db.cursor()
+    num_rows = cur.execute("SELECT cities.id, cities.name, states.name\
+                           FROM cities INNER JOIN states\
+                           ON cities.state_id=states.id\
+                           ORDER BY cities.id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
