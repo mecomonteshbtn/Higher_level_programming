c03d4fe26d9638890e449d4c2d5bc7769521a050
Robinson Montes <1574@holbertonschool.com>
2020-08-09 12:17:29 +0000

    Print the State id with the name passed as argument from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""
+Created on Sat Aug  8 09:05:11 2020
+
+@author: Robinson Montes
+"""
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+import sys
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 5:
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
+    username = args[1]
+    password = args[2]
+    data = args[3]
+    state_name = args[4]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, data))
+    # create custom session object class from database engine
+    Session = sessionmaker(bind=engine)
+    # create instance of new custom session class
+    session = Session()
+    states = session.query(State).filter(State.name == state_name)\
+                    .order_by(State.id)
+    if states is not None and states.count() > 0:
+        for state in states:
+            print('{}'.format(state.id))
+    else:
+        print('Not found')
