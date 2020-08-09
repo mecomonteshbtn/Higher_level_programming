a39f3ad84226fc29d58dbd821fff5e909e3c2034
Robinson Montes <1574@holbertonschool.com>
2020-08-09 11:45:16 +0000

    Use ORM model to list sorted states

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+Created on Sat Aug  8 09:05:11 2020
+
+@author: Robinson Montes
+"""
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+import sys
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    # create custom session object class from database engine
+    Session = sessionmaker(bind=engine)
+    # create instance of new custom session class
+    session = Session()
+    for state in session.query(State).order_by(State.id):
+        print('{}: {}'.format(state.id, state.name))
