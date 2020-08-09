e363e2aac599dea19db7b196caee38deb94f6eb7
Robinson Montes <1574@holbertonschool.com>
2020-08-09 11:28:18 +0000

    Create a Base model inherites from Base alchemy class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+Created on Sat Aug  8 09:05:11 2020
+
+@author: Robinson Montes
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    state class for use with sqlalchemy inherits from sqlalchemy
+    declarative_base
+
+    Attributes:
+        Base (class)
+    """
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
