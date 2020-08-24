4a058eedd113ed93f27f3906805f3dc99fe64c11
Robinson Montes <1574@holbertonschool.com>
2020-08-24 20:31:32 +0000

    fixing code 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,12 +6,15 @@ Created on Mon Aug 24 07:02:53 2020
 @author: Robinson Montes
 """
 from urllib.request import urlopen, Request
+from urllib.parse import urlencode
 from sys import argv
 
 
 if __name__ == '__main__':
     url = argv[1]
     email = {'email': argv[2]}
+    email = urlencode(email)
+    email = email.encode('ascii')
     request = Request(url, email)
     with urlopen(url) as response:
         string = response.read().decode('utf-8')
