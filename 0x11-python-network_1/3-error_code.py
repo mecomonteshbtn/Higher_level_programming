151281b7b6505ae186474a4be533d90e5f8a8951
Robinson Montes <1574@holbertonschool.com>
2020-08-24 13:21:40 +0000

    a Python script that takes in a URL, sends a request to the URL and displays the body of the response.

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 07:02:53 2020
+
+@author: Robinson Montes
+"""
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    try:
+        with urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as e:
+        print('Error code: {}'.format(e.code))
