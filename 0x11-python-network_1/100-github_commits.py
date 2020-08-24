bf46d924a806a99286b35aa5ec211cf11f0a383b
Robinson Montes <1574@holbertonschool.com>
2020-08-24 23:20:36 +0000

    obtain the first 10 commits and name from github

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 07:02:53 2020
+
+@author: Robinson Montes
+"""
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    repo = argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(username, repo)
+    response = get(url)
+    try:
+        obj = response.json()
+        for i in range(10):
+            print('{}: {}'.format(obj[i]['sha'],
+                                  obj[i]['commit']['author']['name']))
+    except ValueError:
+        print('Not a valid JSON')
