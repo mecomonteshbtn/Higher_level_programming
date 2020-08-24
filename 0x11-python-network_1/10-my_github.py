e3a48987c89914a0a959b570caa1fbd3651cb7be
Robinson Montes <1574@holbertonschool.com>
2020-08-24 22:39:47 +0000

    take my github credential to consume the github API

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 07:02:53 2020
+
+@author: Robinson Montes
+"""
+from requests import get
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    username = argv[1]
+    password = argv[2]
+    response = get(url, auth=HTTPBasicAuth(username, password))
+    try:
+        obj = response.json()
+        print(obj.get('id'))
+    except ValueError:
+        print(None)
