1e4fc53b7b1355bf8404a087972d43e7fe4d2170
Robinson Montes <1574@holbertonschool.com>
2020-08-24 12:16:51 +0000

    takes in a URL, sends a request to the URL and displays the value of the X-Request-Id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 07:02:53 2020
+
+@author: Robinson Montes
+"""
+from urllib.request import urlopen
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    with urlopen(url) as response:
+        info = response.info()
+        print(info['X-Request-Id'])
