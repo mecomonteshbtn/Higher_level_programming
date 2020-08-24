ab6c86670c58ed5cd6fce3fc161912053d59ab7f
Robinson Montes <1574@holbertonschool.com>
2020-08-24 19:52:03 +0000

    push'em all

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 07:02:53 2020
+
+@author: Robinson Montes
+"""
+from requests import post
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = {'email': argv[2]}
+    response = post(url, data=email)
+    print(response.text)
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 07:02:53 2020
+
+@author: Robinson Montes
+"""
+from requests import post, codes
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(argv) > 1:
+        q = {'q': argv[1]}
+    else:
+        q = {'q': ''}
+    response = post(url, data=q)
+    if response.status_code == codes.ok:
+        obj = response.json()
+        if len(obj) == 0:
+            print('No result')
+            exit()
+        print('[{}] {}'.format(obj['id'], obj['name']))
+    else:
+        print('No result')
+        exit()
