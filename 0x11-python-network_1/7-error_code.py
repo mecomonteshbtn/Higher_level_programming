bc3757a3a90d81ecccc798645592309cca20c00a
Robinson Montes <1574@holbertonschool.com>
2020-08-24 21:25:52 +0000

    change code.ok for < 400 in 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -12,7 +12,7 @@ from sys import argv
 if __name__ == '__main__':
     url = argv[1]
     response = get(url)
-    if response.status_code == codes.ok:
+    if response.status_code < 400:
         print(response.text)
     else:
         print('Error code: {}'.format(response.status_code))
