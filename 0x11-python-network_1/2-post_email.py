ea92a0080a6fb232da1fd37b9a45914dab89015a
Robinson Montes <1574@holbertonschool.com>
2020-08-24 12:41:48 +0000

    a Python script that fetches https://intranet.hbtn.io/status

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 07:02:53 2020
+
+@author: Robinson Montes
+"""
+from urllib.request import urlopen, Request
+from sys import argv
+
+
+if __name__ == '__main__':
+    url =  argv[1]
+    email = {'email': argv[2]}
+    request = Request(url, email)
+    with urlopen(url) as response:
+        string = response.read().decode('utf-8')
+        print(string)
