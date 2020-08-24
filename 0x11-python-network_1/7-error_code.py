66504085d61bd563095e7e3769085837c9ddb6e8
Robinson Montes <1574@holbertonschool.com>
2020-08-24 13:21:54 +0000

    a Python script that takes in a URL, sends a request to the URL and displays the body of the response.

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 07:02:53 2020
+
+@author: Robinson Montes
+"""
+from requests import get
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    response = get(url)
+    if response.status_code == codes.ok:
+        print(response.text)
+    else:
+        print('Error code: {}'.format(response.status_code))
