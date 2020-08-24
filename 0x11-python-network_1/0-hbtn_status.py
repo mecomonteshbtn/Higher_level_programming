0fa3d50d4256713e1bff641a2cd7327e42fc3c9a
Robinson Montes <1574@holbertonschool.com>
2020-08-24 11:59:16 +0000

    fetches the statu from Holberton intranet

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 05:14:59 2020
+
+@author: meco
+"""
+from urllib.request import urlopen
+
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    with urlopen(url) as response:
+        bytes_content = response.read()
+        content = bytes_content.decode('utf-8')
+        string = """Body response:
+            \t- type: {}
+            \t- content: {}
+            \t- utf8 content: {}""".format(type(bytes_content), bytes_content,
+                                            content)
+        print(string)
