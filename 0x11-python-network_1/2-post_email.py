d1681b16d9d559f7d4cf90bec167aa197aa03e00
Robinson Montes <1574@holbertonschool.com>
2020-08-24 20:36:17 +0000

    just request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,8 +13,6 @@ from sys import argv
 if __name__ == '__main__':
     url = argv[1]
     email = {'email': argv[2]}
-    email = urlencode(email)
-    email = email.encode('ascii')
     request = Request(url, email)
     with urlopen(request) as response:
         string = response.read().decode('utf-8')
