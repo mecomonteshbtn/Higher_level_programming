1077237f1b5c9c188f88fdce525316724275d88b
Robinson Montes <1574@holbertonschool.com>
2020-08-25 01:21:16 +0000

    my first attempt using twitter API

--- 0x11-python-network_1/103-search_twitter.py
+++ 0x11-python-network_1/103-search_twitter.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 07:02:53 2020
+
+@author: Robinson Montes
+"""
+from requests import post, get
+from sys import argv
+from base64 import b64encode
+
+
+if __name__ == '__main__':
+    APIkey = '{}:{}'.format(argv[1], argv[2])
+    base64_encode = b64encode(APIkey.encode()).decode('utf-8')
+    data = {'grant_type': 'client_credentials'}
+    header = {'Authorization': 'Basic {}'.format(base64_encode),
+              'Content-Type':
+              'application/x-www-form-urlencoded;charset=UTF-8'}
+    req = post('https://api.twitter.com/oauth2/token', headers=header,
+               data=data)
+    response = req.json()
+    token = response.get('access_token')
+    header = {'Authorization': 'Bearer {}'.format(token)}
+    data = {'q': argv[3], 'count': 5}
+    req = get('https://api.twitter.com/1.1/search/tweets.json',
+              headers=header, params=data)
+    response = req.json()
+    tweets = response.get('statuses')
+    for tweet in tweets:
+        print('[{}] {} by {}'.format(tweet.get('id'), tweet.get('text'),
+                                     tweet.get('user').get('name')))
