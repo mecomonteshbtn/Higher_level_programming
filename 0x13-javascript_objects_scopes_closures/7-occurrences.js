29113668beafc9f496ea7408730a21527523c3c2
Robinson Montes <1574@holbertonschool.com>
2020-09-21 20:53:25 +0000

    Number of occurences in a list

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let occurences = 0;
+  for (const element of list) {
+    if (element === searchElement) {
+      occurences++;
+    }
+  }
+  return occurences;
+};
