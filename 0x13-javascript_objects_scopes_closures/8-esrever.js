71517cf7bee78c04b6af4ac42393a0c19ded9bf6
Robinson Montes <1574@holbertonschool.com>
2020-09-21 21:49:24 +0000

    reverse a list without using a built function reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const reverse = [];
-  for (let i = list.length - 1; i > 0; --i) {
+  for (let i = list.length - 1; i >= 0; i--) {
     reverse.push(list[i]);
   }
   return reverse;
