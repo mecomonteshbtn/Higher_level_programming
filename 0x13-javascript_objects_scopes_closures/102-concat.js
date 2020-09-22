8aca644398fa97c02c134290634162871f5f849e
Robinson Montes <1574@holbertonschool.com>
2020-09-22 05:18:58 +0000

    concat 2 files in another file called fileC

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const argv = process.argv;
+const file = require('fs');
+const fileA = argv[2];
+const textA = file.readFileSync(fileA, 'utf8');
+const fileB = argv[3];
+const textB = file.readFileSync(fileB, 'utf8');
+const fileC = argv[4];
+file.writeFileSync(fileC, textA + textB);
