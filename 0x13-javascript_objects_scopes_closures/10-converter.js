b1f4a021c28f6065c9ecad02d73740d74ccbf24e
Robinson Montes <1574@holbertonschool.com>
2020-09-22 04:21:54 +0000

    Convert a base 10 number into another inputted base

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
 exports.converter = function converter (base) {
-  if (!converter.Number) {
-    converter.Number = base;
-    return converter;
-  } else {
-    return parseInt(converter.Number, base);
-  }
+  return x => x.toString(base);
 };
