5048b9c8b4596db480e4425a45c177e6650f43f4
Robinson Montes <1574@holbertonschool.com>
2020-09-22 00:43:05 +0000

    Converter base number with parseInt function

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+exports.converter = function converter (base) {
+  if (!converter.Number) {
+    converter.Number = base;
+    return converter;
+  } else {
+    return parseInt(converter.Number, base);
+  }
+};
