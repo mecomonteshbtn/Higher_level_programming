bd6314fc4620fd58b1b020a7992875f5accabfa1
Robinson Montes <1574@holbertonschool.com>
2020-09-22 22:36:04 +0000

    All the task until 4

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-let url = process.argv[2];
+const url = process.argv[2];
 
 request(url, function (error, response, body) {
   if (error != null) {
