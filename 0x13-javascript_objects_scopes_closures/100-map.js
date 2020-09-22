323b77233f42822c64a3b0dcc9f797f6b3d1b425
Robinson Montes <1574@holbertonschool.com>
2020-09-22 04:44:11 +0000

    Compute a new array multiply each element of the array by its index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const array = require('./100-data').list;
 console.log(array);
-console.log(array.map(x => x * array.indexOf(x));
+console.log(array.map(x => x * array.indexOf(x)));
