305eebc39bdf90f04fd627b6d40dde36ba521926
Robinson Montes <1574@holbertonschool.com>
2020-09-23 00:56:34 +0000

    Compute the number of tasks completed by user id. The first argument is the API URL: https://jsonplaceholder.typicode.com/todos

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+let data;
+const dictionary = {};
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    data = JSON.parse(body);
+    data.forEach(function (result) {
+      if (result.completed === true) {
+        const userid = result.userId;
+        if (!(userid in dictionary)) {
+          dictionary[userid] = 0;
+        }
+        dictionary[userid] += 1;
+      }
+    });
+    console.log(dictionary);
+  }
+});
