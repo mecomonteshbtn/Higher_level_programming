38bb227ed3f29540701dcbe2bd65596333ef4774
Robinson Montes <1574@holbertonschool.com>
2020-09-25 04:44:23 +0000

    Show all the Star Wars film names from swapi api

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+const $ = window.$;
+const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+
+$.getJSON(url, function (body) {
+  let name = body.name;
+  $('DIV#character').text(name);
+});
