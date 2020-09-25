ff8699acf25b2f34c0c683b5dc014de2bc84e262
Robinson Montes <1574@holbertonschool.com>
2020-09-25 04:49:21 +0000

    Consume an API to show tha key name

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+const $ = window.$;
+const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
+
+$.getJSON(url, function (body) {
+  const hello = body.hello;
+  $('DIV#hello').text(hello);
+});
